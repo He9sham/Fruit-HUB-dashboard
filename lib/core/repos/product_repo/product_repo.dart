5d317816a7f9e6c63ae91449db8cff3f25e13e83
hesha30m <95132216+He9sham@users.noreply.github.com>
2024-12-27 16:52:13 +0200

    fix code import

--- lib/core/repos/product_repo/product_repo.dart
+++ lib/core/repos/product_repo/product_repo.dart
@@ -1,4 +1,6 @@
-import 'package:commerce_hub_dashboard/features/add_product/domain/entities/add_product_input_entitie.dart';
+
+
+import 'package:commerce_hub_dashboard/features/add_product/domain/entities/add_product_input_entity.dart';
 
 abstract class ProductRepo {
   Future<void> addProduct(AddProductInput addProductInput);
