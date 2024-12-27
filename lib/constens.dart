84caa2d8f241c0f3a62bcc91dfeb67315f3e36b1
hesha30m <95132216+He9sham@users.noreply.github.com>
2024-12-27 21:07:01 +0200

    add supabase to my project

---
+++ lib/constens.dart
@@ -0,0 +1,3 @@
+const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4dnRpdnF0Z3dvb3NrY2JqbWhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUzMjU5MTEsImV4cCI6MjA1MDkwMTkxMX0.T2ThB2rWe0_kh7EJRYqkmI2emKY13QN4erwTWNXsBvA';
+
+const String profileUrl = 'https://yxvtivqtgwooskcbjmhs.supabase.co';
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:commerce_hub_dashboard/constens.dart';
 import 'package:commerce_hub_dashboard/core/utils/app_route.dart';
 import 'package:commerce_hub_dashboard/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   await ScreenUtil.ensureScreenSize();
+   await Supabase.initialize(
+    url: profileUrl,
+    anonKey: anonKey,
+  );
   runApp(
     Dashboard(
       appRouter: AppRouter(),
