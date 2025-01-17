c7b557abfe6f09251a45ac40d40feab8c601065c
hesha30m <95132216+He9sham@users.noreply.github.com>
2025-01-17 16:48:51 +0200

    add review features

---
+++ lib/features/add_product/data/models/review_models.dart
@@ -0,0 +1,14 @@
+class ReviewModels {
+  final String name;
+  final String image;
+  final String reviewDescription;
+  final int rating;
+  final String date;
+
+  ReviewModels(
+      {required this.name,
+      required this.image,
+      required this.reviewDescription,
+      required this.rating,
+      required this.date});
+}
---
+++ lib/features/add_product/domain/entities/review_entity.dart
@@ -0,0 +1,14 @@
+class ReviewEntity {
+  final String name;
+  final String image;
+  final String reviewDescription;
+  final int rating;
+  final String date;
+
+  ReviewEntity(
+      {required this.name,
+      required this.image,
+      required this.reviewDescription,
+      required this.rating,
+      required this.date});
+}
